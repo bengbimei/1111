e0baa285e9a97bb756da81f5e9bac6eff1d28720
lty1 <lengtianyou@163.com>
2015-04-28 10:52:51 +0800

    修改了

--- app/src/main/java/com/example/aaa/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aaa/myapplication/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
 
 
         return true;
-        
+
     }
 
     @Override
@@ -38,6 +38,7 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
